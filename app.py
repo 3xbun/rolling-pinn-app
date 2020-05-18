96de7ebb7bb4ce578ce7d2798014778eecffa81a
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2020-05-19 03:16:59 +0700

    Fix bug

--- app.py
+++ app.py
@@ -4,4 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    greeting = [
+        "Hello! What would you like to order ka?"
+        ]
+    thankyou = [
+        "Thank you ka!\nWe hope you will enjoy our sweets ka! ❤️❤️❤️",
+        "Thank you for your support ka! Let us know what you think of our sweets ❤️ we hope you will enjoy it 😍😍😍",
+        "Thank you for your lovely reviews today ❤️❤️❤️ we are glad to deliver some #cookieclimax to your home 💋💋💋 🥰🥰🥰. Order some cookies to get your ultimate #cookieclimax!! Just DM us ❤️❤️❤️."
+    ]
+
+    return render_template('index.html', greeting=greeting, thankyou=thankyou)
+
+
+    greeting = []
