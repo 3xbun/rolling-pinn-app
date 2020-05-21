f4087f079f7c817058d98382e0741905619400ff
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2020-05-21 13:36:50 +0700

    Update value on ind cake

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ def index():
     thankyou = [
         "Thank you ka!\nWe hope you will enjoy our sweets ka! ❤️❤️❤️",
         "Thank you for your support ka! Let us know what you think of our sweets ❤️ we hope you will enjoy it 😍😍😍",
-        "Thank you for your lovely reviews today ❤️❤️❤️ we are glad to deliver some #cookieclimax to your home 💋💋💋 🥰🥰🥰. Order some cookies to get your ultimate #cookieclimax!! Just DM us ❤️❤️❤️.",
         "Let us know if u would like some na ka. \nIt’s very good ka ♥️",
         "Please let us know if you’re interested in other treats too na ka\nThank you! Ka"
     ]
